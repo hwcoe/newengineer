83a5fd7bfd8bce0df12f81d303820faeaa5aeb9d
sezj <sarahzachrich@gmail.com>
2017-09-29 16:01:02 -0400

    gruntification of project

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+	 pkg: grunt.file.readJSON('package.json'),
+
+	 /**
+	  * Sass tasks
+	  */
+	  sass: {
+		dist: {
+		  options: {
+			 style: 'compressed'
+		  },
+		  files: {
+			 './style.css' : 'sass/style.scss',
+			 './editor-style.css' : 'sass/editor-style.scss' 
+		  } 
+		}  
+	  },
+
+		/**
+	  * Autoprefixer
+	  */
+	  postcss: {
+		options: {
+		  map: {
+			 inline: false 
+		  },
+		  processors: [
+			 require('autoprefixer')({browsers: ['last 2 versions']})
+		  ]
+		},
+		// prefix all css files in the project root
+		dist: {
+		  src: './*.css'
+		}  
+	  },
+
+	  /**
+		 * Watch task
+		 */
+		 watch: {
+			grunt: {
+				files: ['Gruntfile.js'],
+				options: {
+					reload: true
+				}
+			},
+			css: {
+				files: ['./sass/*.scss'],
+				tasks: ['sass', 'postcss']
+			} 
+		 }
+
+	 
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-postcss');
+  // Default task(s).
+
+  grunt.registerTask('default',['watch']);
+
+};
